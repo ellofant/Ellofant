65071478d8d67fa2333b8f9459d7871623f33c6a
Alonso Nunez <alonsonunez@protonmail.com>
2024-09-21 16:57:20 -0500

    add github actions workflow for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+
 export default defineConfig({
   output: 'server',
+  site: 'https://github.com/yamadashy/repopack.git',
+  base: 'astro-app',
   
 });
