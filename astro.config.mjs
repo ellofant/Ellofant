be9e1ed9236db5e96d2a4850be21088aad18a7ae
Alonso Nunez <alonsonunez@protonmail.com>
2024-09-22 09:09:17 -0500

    fixed client side route handling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 
-export default defineConfig({
-  site: 'https://alonso510.github.io',
-  base: '/astro-app',
-  // outDir is not specified, so it will use the default 'dist'
-});
+   export default defineConfig({
+     site: 'https://alonso510.github.io',
+     base: '/astro-app',
+     build: {
+       assets: 'astro'
+     }
+   });
