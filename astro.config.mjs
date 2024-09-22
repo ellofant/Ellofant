72ba7771af6a4763f9578bb5c00f8dd538b7d8fd
Alonso Nunez <alonsonunez@protonmail.com>
2024-09-22 09:43:03 -0500

    Fix navbar links to use correct URL structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
 
-   export default defineConfig({
-     site: 'https://alonso510.github.io',
-     base: '/astro-app',
-     build: {
-       assets: 'astro'
-     }
-   });
+export default defineConfig({
+  site: 'https://alonso510.github.io',
+  base: '/astro-app',
+  trailingSlash: 'always', // This ensures all URLs have a trailing slash
+});
