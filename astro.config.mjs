2cfde7d895102b8bcf1c33999bc30e4ccf980955
Alonso Nunez <alonsonunez@protonmail.com>
2024-09-23 13:55:07 -0500

    fixed logo issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,13 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://alonso510.github.io',
-  base: '/astro-app',
   trailingSlash: 'always', // This ensures all URLs have a trailing slash
+  base: '/astro-app',
+  build: {
+    assets: '_assets'
+  }
 });
+
+
+
+
