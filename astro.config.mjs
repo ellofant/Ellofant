a173155317598c881657fa93eb8172d3e5e3f1e1
Alonso Nunez <alonsonunez@protonmail.com>
2024-10-04 15:22:02 -0500

    Update GitHub Actions workflow and disable Jekyll

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://alonso510.github.io',
   trailingSlash: 'always', // This ensures all URLs have a trailing slash
-  base: '/astro-app',
+  base: '/Ellofant',
   build: {
     assets: '_assets'
   }
